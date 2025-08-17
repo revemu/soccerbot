41a473206e6660e9f92f471075044fc03b5ea996
revemu <128660623+revemu@users.noreply.github.com>
2025-08-17 12:35:32 +0700

    Update hook.php

--- hook.php
+++ hook.php
@@ -25,7 +25,7 @@ require_once('./cmd.php');
 require_once("./match_query.php");
 require_once("./flex.php");
 require_once("./member.php");
-require("./config.php");
+include "./config.inc" ;
 require __DIR__ . '/vendor/autoload.php';
 
 
